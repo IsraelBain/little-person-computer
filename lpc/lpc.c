2223a6164cb2863164c165ce906fc9ac16fc8c2b
Israel Bain <ibain@timberlea.CS.Dal.Ca>
2024-11-28 23:42:48 -0400

    i cant fix this in time:(

--- lpc/lpc.c
+++ lpc/lpc.c
@@ -1,6 +1,169 @@
 #include <stdio.h>
 #include "lpc.h"
+//from .h file
+int memory[MEMORY_SIZE];
+int accumulator;
+int programCounter;
+int instructionReg;
+int opCode;
+int operand;
+
+void initialize_memory(){
+    for(int i = 0; i < MEMORY_SIZE; i++){
+        memory[i] = 0;
+    }
+}
+
+void initialize_registers(){
+    accumulator = 0;
+    programCounter = 0;
+    instructionReg = 0;
+    opCode = 0;
+    operand = 0;
+}
+
+void load_program(){
+    int instruction, index = 0;
+
+    while(scanf("%d", &instruction) != EOF){
+        if(instruction == SENTINEL) break;
+        if(instruction < MIN_VALUE || instruction > MAX_VALUE){
+            printf("***INVALID INSTRUCTION: %d***\n", instruction);
+            continue;
+        }
+
+
+        if(index < MEMORY_SIZE){
+            memory[index++] = instruction;
+        }else{
+            break;
+        }
+    }
+}
+void memory_dump() {
+    printf("\nRegisters: \n");
+    printf("accumulator %+05d\n", accumulator);
+    printf("program counter %05d\n", programCounter);
+    printf("op-code %05d\n", opCode);
+    printf("operand %05d\n", operand);
+
+    printf("\n      0    1    2    3    4    5    6    7    8    9 \n");
+    for (int i = 0; i < MEMORY_SIZE; i++) {
+        if (i % 10 == 0) {
+            if (i > 0) printf("\n");
+            printf("%2d ", (i / 10) * 10);
+        }
+        printf("%+05d ", memory[i]);
+    }
+    printf("\n");
+}
+void execute_program(){
+    printf("***Start LPC Execution***\n");
+    while(1){
+        instructionReg = memory[programCounter];
+        opCode = instructionReg / 100;
+        operand = instructionReg % 100;
+
+        switch (opCode) {
+            case READ:
+                if(scanf("%d", &memory[operand]) == EOF ){
+                    printf("***FATAL ERROR - Unexpected EOF during READ***\n");
+                    printf("***LPC execution abnormally terminated***\n");
+                    memory_dump();
+                    return;
+                }
+                programCounter++;
+                break;
+
+            case WRITE:
+                printf("Contents of %02d: %d\n", operand, memory[operand]);
+                programCounter++;
+                break;
+
+            case LOAD:
+                accumulator = memory[operand];
+                programCounter++;
+                break;
+
+            case STORE:
+                memory[operand] = accumulator;
+                programCounter++;
+                break;
+
+            case ADD:
+                accumulator += memory[operand];
+                programCounter++;
+                break;
+
+            case SUBTRACT:
+                accumulator -= memory[operand];
+                programCounter++;
+                break;
+
+            case DIVIDE:
+                if (memory[operand] == 0){
+                    printf("***FATAL ERROR - Attempt to divide by zero***\n");
+                    printf("***LPC execution abnormally terminated***\n");
+                    memory_dump();
+                    return;
+                }
+                accumulator /= memory[operand];
+                programCounter++;
+                break;
+
+            case MULTIPLY:
+                accumulator *= memory[operand];
+                programCounter++;
+                break;
+
+            case JUMP:
+                programCounter = operand;
+                break;
+
+            case JUMPNEG:
+                if (accumulator < 0)
+                    programCounter = operand;
+                else
+                    programCounter++;
+                break;
+
+            case JUMPZERO:
+                if (accumulator == 0)
+                    programCounter = operand;
+                else
+                    programCounter++;
+                break;
+
+            case HALT:
+                printf("***End LPC Execution***\n");
+                return;
+
+            default:
+                printf("***FATAL ERROR: Illegal opCode***\n");
+                printf("***LPC execution abnormally terminated***\n");
+                memory_dump();
+                return;
+
+        }
+
+        //overflow
+        if (accumulator < MIN_VALUE || accumulator > MAX_VALUE) {
+            printf("***FATAL ERROR - Accumulator Overflow***\n");
+            printf("***LPC execution abnormally terminated***\n");
+            memory_dump();
+            return;
+        }
+    }
+}
+
+
+
 
 int main() {
+    initialize_memory();
+    initialize_registers();
+    load_program();
+    execute_program();
     return 0;
 }
+
--- lpc/lpc.h
+++ lpc/lpc.h
@@ -1,4 +1,45 @@
-#ifndef lpc_h
-#define lpc_h
+#ifndef LPC_H
+#define LPC_H
+//MEMORY SIZE
+#define MEMORY_SIZE 100
+#define MIN_VALUE -9999
+#define MAX_VALUE 9999
+//OPCODE
+#define READ 10
+#define WRITE 11
+#define LOAD 20
+#define STORE 21
+#define ADD 30
+#define SUBTRACT 31
+#define DIVIDE 32
+#define MULTIPLY 33
+#define JUMP 40
+#define JUMPNEG 41
+#define JUMPZERO 42
+#define HALT 43
+//ERROR MESSAGES
+#define ERR_DIV_BY_ZERO "***FATAL ERROR - ATTEMPT TO DIVIDE BY ZERO***"
+#define ERR_INVALID_OPCODE "***FATAL ERROR - INVALID OPCODE***"
+#define ERR_OVERFLOW "***FATAL ERROR - ACCUMULATOR OVERFLOW***"
 
-#endif lpc_h
+#define SENTINEL -99999
+#define TRUE 1
+#define FALSE 0
+
+extern int memory[MEMORY_SIZE];
+extern int accumulator;
+extern int programCounter;
+extern int instructionReg;
+extern int opCode;
+extern int operand;
+
+void initialize_memory();
+void initialize_registers();
+void load_program();
+void execute_program();
+void memory_dump();
+void handle_error(const char *error_message);
+
+
+
+#endif
